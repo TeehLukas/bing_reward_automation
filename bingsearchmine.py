3a310bb577f9c1296d17b0343d31d0a81b1c5e5f
TeehLukas <lukas.cakiroglu@gmail.com>
2025-01-16 21:24:09 +0100

    - Added Modulo to don't get: index out of range, Error - Added 30 to searches, because the daily cap is at 90 points. 30 x 3 = 90 - Under 5 sec sleep, the counter on Bing doesn't increase - Some Formatting for search_queries

--- bingsearchmine.py
+++ bingsearchmine.py
@@ -2,26 +2,30 @@ import webbrowser
 import time
 import pyautogui
 import random
-# Liste der Suchanfragen
-search_queries = ['apple','banana','orange','grape','watermelon','pineapple','blueberry','strawberry','kiwi','mango','peach','pear','plum','cherry','apricot','nectarine','raspberry','blackberry','fig','pomegranate','lemon','lime','coconut','papaya','guava','persimmon','lychee','dragonfruit','passionfruit','cranberry','cucumber','tomato','potato','onion','garlic','ginger','carrot','broccoli','spinach','kale','lettuce','celery','parsley','coriander','basil','thyme','rosemary','oregano','dill','chive','sage','mint','lavender','bayleaf','artichoke','asparagus','beetroot','cauliflower','zucchini','squash','eggplant','pepper','chili','radish','turnip','parsnip','cabbage','brusselsprout','leek','scallion','mushroom','peanut','almond','cashew','pistachio','walnut','hazelnut','pecan','chestnut','macadamia','brazilnut','sunflower','pumpkinseed','sesameseed','chia','flaxseed','quinoa','barley','oat','wheat','corn','rice','millet','buckwheat','amaranth','teff','soybean','lentil','chickpea','bean','peas'
-]
 
-# Anzahl der Suchanfragen
+search_queries = ['apple','banana','orange','grape','watermelon','pineapple','blueberry','strawberry','kiwi','mango','peach','pear',
+                  'plum','cherry','apricot','nectarine','raspberry','blackberry','fig','pomegranate','lemon','lime','coconut','papaya',
+                  'guava','persimmon','lychee','dragonfruit','passionfruit','cranberry','cucumber','tomato','potato','onion','garlic',
+                  'ginger','carrot','broccoli','spinach','kale','lettuce','celery','parsley','coriander','basil','thyme','rosemary',
+                  'oregano','dill','chive','sage','mint','lavender','bayleaf','artichoke','asparagus','beetroot','cauliflower','zucchini',
+                  'squash','eggplant','pepper','chili','radish','turnip','parsnip','cabbage','brusselsprout','leek','scallion','mushroom',
+                  'peanut','almond','cashew','pistachio','walnut','hazelnut','pecan','chestnut','macadamia','brazilnut','sunflower','pumpkinseed',
+                  'sesameseed','chia','flaxseed','quinoa','barley','oat','wheat','corn','rice','millet','buckwheat','amaranth','teff',
+                  'soybean','lentil','chickpea','bean','peas']
+
+bing = "https://www.bing.com"
 num_searches = 30
 
-# Bing-Such-URL
-bing_url = "https://www.bing.com"
-
-# Schleife, um die Suchanfragen auszuführen
 for i in range(num_searches):
-    eins = search_queries[i+random.randint(1,len(search_queries))]
-    zwei = search_queries[i+random.randint(1,len(search_queries))]
+    eins_index = (i + random.randint(1, len(search_queries) - 1)) % len(search_queries)
+    zwei_index = (i + random.randint(1, len(search_queries) - 1)) % len(search_queries)
+    eins = search_queries[eins_index]
+    zwei = search_queries[zwei_index]
     query = eins + " " + zwei
-    print(query)
-    webbrowser.open_new_tab(bing_url)
-    time.sleep(3)  # Wartezeit, um sicherzustellen, dass der Tab geöffnet istpea
-    pyautogui.write(query, interval=0.1)  # Zeichen für Zeichen eingeben
+    webbrowser.open_new_tab(bing)
+    time.sleep(3)  
+    pyautogui.write(query, interval=0.1) 
     pyautogui.press('enter')
-    time.sleep(2)  # Wartezeit zwischen den Suchanfragen (in Sekunden)
+    time.sleep(2)  
 
-#print("Suchanfragen abgeschlossen!")
+print("Search requests finished!")
